b699dcea639fdaae73e7a6a20d498f7f0e77d826
MelLeyva <mleyva0795@gmail.com>
2021-07-20 16:57:25 -0500

    Filtrado

---
+++ src/data.js
@@ -0,0 +1,20 @@
+// estas funciones son de ejemplo
+
+//import athletes from "./data/athletes/athletes.js";
+import data from './data/athletes/athletes.js';
+
+export const name = data.athletes.sort((a,b) => {
+  return (a.name < b.name) ? - 1 : 1
+})
+ //console.log(name);
+
+/*export const sport = data.athletes.filter(function(spt) {
+  return spt.sport == "Volleyball";
+});
+*/
+export function paisito(pais){
+  let resultadito=data.athletes.filter(function(team) {
+  return team.team == pais;
+})
+return resultadito
+}
--- src/main.js
+++ src/main.js
@@ -1,1 +1,63 @@
+import {name,paisito} from './data.js';
+import athletes from './data/athletes/athletes.js';
+import data from './data/athletes/athletes.js';
 
+/*let titulo = document.getElementById("olympics-title");
+*/
+let divAthletes =  document.getElementById("athletes");
+
+let selectPais = document.getElementById("pais")
+
+
+selectPais.addEventListener("change",function(e){
+    divAthletes.innerHTML = "";
+    paisito(e.target.value).forEach(a => divAthletes.appendChild(obtenerElementoAtleta(a)));
+
+});
+
+function obtenerElementoAtleta(atleta){
+    const ul = document.createElement("ul");
+    const aName = document.createElement("li");
+        aName.innerHTML = "Nombre: " + atleta.name;
+    const aSport = document.createElement("li");
+        aSport.innerHTML = atleta.sport;
+    const aTeam = document.createElement("li");
+        aTeam.innerHTML = atleta.team;
+    const aEvent = document.createElement("li");
+        aEvent.innerHTML = atleta.event;
+    const aMedal = document.createElement("li");
+        aMedal.innerHTML = atleta.medal;
+    const aGender = document.createElement("li");
+        aGender.innerHTML = atleta.gender;
+    const aAge = document.createElement("li");
+        aAge.innerHTML = atleta.age;
+    const aWeight = document.createElement("li");
+        aWeight.innerHTML = atleta.weight;
+    const aHeight = document.createElement("li");
+        aHeight.innerHTML = atleta.height;
+    const aNoc = document.createElement("li");
+        aNoc.innerHTML = atleta.noc;    
+    ul.appendChild(aName);
+    ul.appendChild(aAge);
+    ul.appendChild(aGender);
+    ul.appendChild(aAge);
+    ul.appendChild(aWeight);
+    ul.appendChild(aHeight);
+    ul.appendChild(aTeam);
+    ul.appendChild(aNoc);
+    ul.appendChild(aSport);
+    ul.appendChild(aEvent);
+    ul.appendChild(aMedal);
+    return ul;
+}
+
+/*titulo.innerHTML = data.games + " Ciudad: "+data.city;
+let cards= function showData () {
+    data.athletes.forEach(function(atleta){
+    
+   
+        divAthletes.appendChild(ul);
+    })
+}
+console.log(cards);
+*/
